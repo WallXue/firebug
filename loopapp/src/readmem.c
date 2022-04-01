1e0ae124ab049770a359b34d5afc5623388ecbcb
xuewu <qinglan.xw@raycloud.com>
2022-04-01 14:31:46 +0800

    动态修改程序代码

--- loopapp/src/readmem.c
+++ loopapp/src/readmem.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <fcntl.h>
+
+
+#ifndef BYTE
+#define BYTE unsigned char
+#endif
+
 
 int getPID(char *pkgName) {
     int pid = -1;
@@ -51,9 +60,46 @@ long getModuleBase(int pid, char *module_name) {
     return address;
 }
 
+void readMem(int pid, long address, unsigned int len, BYTE *bytes) {
+    char memPath[200];
+    sprintf(memPath, "/proc/%d/mem", pid);
+    int handle = open(memPath, O_RDWR);
+    pread64(handle, bytes, len, address);
+}
+void writeMem(int pid, long address, unsigned int len, BYTE *bytes) {
+    char memPath[200];
+    sprintf(memPath, "/proc/%d/mem", pid);
+    int handle = open(memPath, O_RDWR);
+    pwrite64(handle, bytes, len, address);
+}
+
+void printCharInHexadecimal(const BYTE *str, int len) {
+    for (int i = 0; i < len; ++ i) {
+        uint8_t val = str[i];
+        char tbl[] = "0123456789ABCDEF";
+        printf("0x");
+        printf("%c", tbl[val / 16]);
+        printf("%c", tbl[val % 16]);
+        printf(" ");
+    }
+    printf("\n");
+}
+
 int main() {
-    int pid = getPID("LoopApp");
-    long moduleBase = getModuleBase(pid, "LoopApp");
-    printf("pid=%d, moduleBase=%lx", pid, moduleBase);
+    char *appName = "LoopApp_linux_x86_64";
+    char *procName = malloc(200);
+    strcpy(procName, "./");
+    strcat(procName, appName);
+    int pid = getPID(procName);
+    long moduleBase = getModuleBase(pid, appName);
+    printf("pid=%d, moduleBase=%lx\n", pid, moduleBase);
+    long opCodeAddr = moduleBase + 0x64C;
+    printf("opCodeAddr=%lx\n", opCodeAddr);
+    BYTE readMemOpCode[4];
+    readMem(pid, opCodeAddr, 4, readMemOpCode);
+    printCharInHexadecimal(readMemOpCode, 4);
+    //修改指令,改变程序运行逻辑
+    readMemOpCode[3] = 0x00;
+    writeMem(pid, opCodeAddr, 4, readMemOpCode);
     return 1;
 }
